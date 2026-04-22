3e03c5e4c1f1deda253208edbe18c17bdc46d449
DuckApprenticeLab <a3155500@adelaide.edu.au>
2026-04-22 19:11:07 +0930

    Move sign up form in sign in page

--- frontend/src/pages/auth/signin/SignInPage.tsx
+++ frontend/src/pages/auth/signin/SignInPage.tsx
@@ -1,71 +1,16 @@
-import styles from "../auth.module.css";
+import SignUpForm from "../signup/SignUpForm";
 
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import type { Dispatch, SetStateAction } from 'react'
-import type { User } from '../../../App'
-
-type Props = {
-  setUser: Dispatch<SetStateAction<User | null>>
+interface SignupPageProps {
+  loadUserProfile: () => void;
 }
 
-function SignInPage({ setUser }: Props) {
-  const navigate = useNavigate()
-
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
-  const handleLogin = () => {
-    const users: User[] = JSON.parse(localStorage.getItem('users') || '[]')
-
-    const foundUser = users.find(u => u.email === email && u.password === password)
-
-    if (foundUser) {
-      localStorage.setItem('currentUser', JSON.stringify(foundUser))
-      setUser(foundUser)
-      navigate('/')
-    } else {
-      alert('Invalid credentials')
-    }
-  }
+function SignupPage({ loadUserProfile }: SignupPageProps) {
 
   return (
-    <div className={styles.authContainer}>
-      <div className={styles.authCard}>
-        <h2>Sign in</h2>
+    <>
+      <SignUpForm loadUserProfile={loadUserProfile} />
+    </>
+  );
+};
 
-        <div className={styles.authForm}>
-          <div className={styles.formGroup}>
-            <label>Email</label>
-            <input
-              type="email"
-              placeholder="Enter your email"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
-          </div>
-
-          <div className={styles.formGroup}>
-            <label>Password</label>
-            <input
-              type="password"
-              placeholder="Enter your password"
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-            />
-          </div>
-
-          <button className={styles.authBtn} onClick={handleLogin}>
-            Log in
-          </button>
-
-          <button className={styles.authBtn} onClick={() => navigate('/register')}>
-            Sign Up
-          </button>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export default SignInPage
+export default SignupPage;
--- frontend/src/pages/auth/signup/SignUpPage.tsx
+++ frontend/src/pages/auth/signup/SignUpPage.tsx
@@ -1,90 +1,16 @@
-import styles from "../auth.module.css";
+import SignUpForm from "../signup/SignUpForm";
 
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import type { Dispatch, SetStateAction } from 'react'
-import type { User } from '../../../App'
-
-type Props = {
-  setUser: Dispatch<SetStateAction<User | null>>
+interface SignupPageProps {
+  loadUserProfile: () => void;
 }
 
-function SignUpPage({ setUser }: Props) {
-  const navigate = useNavigate()
-
-  const [email, setEmail] = useState('')
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-
-  const handleRegister = () => {
-    if (password !== confirmPassword) {
-      alert('Passwords do not match')
-      return
-    }
-
-    const newUser: User = {
-      email,
-      username,
-      password,
-      // 🔥 ONLY admin gets admin
-      role: email === 'admin@gmail.com' ? 'admin' : 'user',
-    }
-
-    // DB REPLACE
-    const users: User[] = JSON.parse(localStorage.getItem('users') || '[]')
-
-    if (users.find(u => u.email === email)) {
-      alert('User already exists')
-      return
-    }
-
-    const updatedUsers = [...users, newUser]
-
-    localStorage.setItem('users', JSON.stringify(updatedUsers))
-    localStorage.setItem('currentUser', JSON.stringify(newUser))
-
-    setUser(newUser)
-    navigate('/')
-  }
+function SignupPage({ loadUserProfile }: SignupPageProps) {
 
   return (
-    <div className={styles.authContainer}>
-      <div className={styles.authCard}>
-        <h2>Create Account</h2>
+    <>
+      <SignUpForm loadUserProfile={loadUserProfile} />
+    </>
+  );
+};
 
-        <div className={styles.authForm}>
-          <div className={styles.formGroup}>
-            <label>Email</label>
-            <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-          </div>
-
-          <div className={styles.formGroup}>
-            <label>Username</label>
-            <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-          </div>
-
-          <div className={styles.formGroup}>
-            <label>Password</label>
-            <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-          </div>
-
-          <div className={styles.formGroup}>
-            <label>Confirm Password</label>
-            <input
-              type="password"
-              value={confirmPassword}
-              onChange={e => setConfirmPassword(e.target.value)}
-            />
-          </div>
-
-          <button className={styles.authBtn} onClick={handleRegister}>
-            Create Account
-          </button>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export default SignUpPage
+export default SignupPage;
