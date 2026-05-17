6ade9a2017482241884bf4dc064cbcfbe4ad8eac
caseyfaggion <fagck001@mymail.unisa.edu.au>
2026-05-17 19:53:15 +0930

    refactored signin flow
    Took Thanhs sign in logic from main and refactored to use the LoadUserProfile.
    Also copied over register.module.css from main for styling.

--- frontend/src/pages/auth/signin/SignInPage.tsx
+++ frontend/src/pages/auth/signin/SignInPage.tsx
@@ -1,22 +1,25 @@
-import { Link } from "react-router";
-import SignInForm from "./SignInForm";
+import { Link } from 'react-router-dom';
+import LoginForm from './SignInForm';
+import styles from '../../Register.module.css';
 
-interface SignupPageProps {
+interface LoginPageProps {
   loadUserProfile: () => void;
 }
 
-function SignupPage({ loadUserProfile }: SignupPageProps) {
-
+function SignInPage({ loadUserProfile }: LoginPageProps) {
   return (
-    <>
-      <SignInForm loadUserProfile={loadUserProfile} />
-      <br />
-      <br />
-      <br />
-      <Link to="/signup">Don't have an account sign-up here!</Link>
-      <Link to="/">Go home!</Link>
-    </>
+    <div className={styles.registerContainer}>
+      <div className={styles.registerCard}>
+        <h2>LOG IN</h2>
+        <div className={styles.registerForm}>
+          <LoginForm loadUserProfile={loadUserProfile} />
+        </div>
+        <br />
+        <br />
+        <Link to="/">Go home!</Link>
+      </div>
+    </div>
   );
-};
+}
 
-export default SignupPage;
+export default SignInPage;
