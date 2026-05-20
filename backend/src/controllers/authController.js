e5468658d523650a87ca27d86f69e1341fdb9a31
timothieecantcode <timothienguyen@gmail.com>
2026-05-20 12:54:24 +0930

    Add hero section, cards section, connect navbar to MSQL server, add navigations to Navbar

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -113,6 +113,9 @@ const signin = async (req, res) => {
         user: {
           id: user.userId,
           email: user.email,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          role: user.role,
         },
         token,
       },
